6467a72e7d5b360067eefaed95d512aa8ad9a0b1
jakob-lindelof <jakob.lindelof@edu.futuregames.nu>
2024-10-04 16:31:39 +0200

    dummy UI concepts + mood meter inital commit

---
+++ Mood/Source/Mood/UI/MoodMoodMeterWidget.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MoodMoodMeterWidget.generated.h"
+
+class UTextBlock;
+class USlider;
+
+UCLASS()
+class UMoodMoodMeterWidget : public UUserWidget
+{
+public:
+
+	UPROPERTY(meta = (BindWidget), BlueprintReadWrite, EditAnywhere)
+	USlider* MoodMeterInnerCircle;
+
+	UPROPERTY(meta = (BindWidget), BlueprintReadWrite, EditAnywhere)
+	USlider* MoodMeterMiddleCircle;
+
+	UPROPERTY(meta = (BindWidget), BlueprintReadWrite, EditAnywhere)
+	USlider* MoodMeterOuterCircle;
+
+	UPROPERTY(meta = (BindWidget), BlueprintReadWrite, EditAnywhere)
+	UTextBlock* MoodMeterNumber;
+	
+};
