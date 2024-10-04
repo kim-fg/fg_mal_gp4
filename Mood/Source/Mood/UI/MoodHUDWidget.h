26ba44b342886b2e89ce09b20aac4eaf9efc2a26
jakob-lindelof <jakob.lindelof@edu.futuregames.nu>
2024-10-04 11:27:47 +0200

    root folder update PER FORS

---
+++ Mood/Source/Mood/UI/MoodHUDWidget.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MoodHUDWidget.generated.h"
+
+
+class UMoodWinScreen;
+class UMoodLostScreen;
+class UMoodPlayerHealthBar;
+class UMoodHealthComponent;
+struct FInputModeUIOnly;
+
+UCLASS()
+
+class UMoodHUDWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	
+	UPROPERTY(meta = (BindWidget))
+	UMoodPlayerHealthBar* HealthBar;
+
+	UPROPERTY()
+	UMoodHealthComponent* HealthComponent;
+
+	UPROPERTY(meta = (BindWidget))
+	UMoodLostScreen* LostScreen;
+
+	UPROPERTY(meta = (BindWidget))
+	UMoodWinScreen* WinScreen;
+
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+	void GetHealthComponent();
+
+	UFUNCTION(Blueprintable)
+	void DisplayLostScreen();
+
+	virtual void NativeConstruct() override;
+
+	
+};
