d514515a79cb97ead1c9d80eeef59f0a7f5e789f
jakob-lindelof <jakob.lindelof@edu.futuregames.nu>
2024-10-15 12:06:33 +0200

    mood stage UI fullscreen indication initial commit + restart level in pause menu fix

---
+++ Mood/Source/Mood/UI/MoodMoodStage.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MoodMoodStage.generated.h"
+
+class UImage;
+
+UCLASS()
+class UMoodMoodStage : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(meta = (BindWidget), BlueprintReadWrite);
+	UImage* Face;
+
+	UPROPERTY(meta = (BindWidget), BlueprintReadWrite);
+	UImage* Face2;
+
+	UPROPERTY(meta = (BindWidget), BlueprintReadWrite);
+	UImage* StageNumber;
+
+protected:
+
+
+private:
+
+
+};
